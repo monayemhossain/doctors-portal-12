50dd143743d8d477f107c61ac5360fea69aa790a
monayemhossain <monayemhossain347.com>
2022-01-22 00:15:10 +0600

    doctors portal m 72.5 completed

--- doctors-portal-server/index.js
+++ doctors-portal-server/index.js
@@ -1,36 +1,47 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-require('dotenv').config();
-const { MongoClient } = require('mongodb');
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
+const { application } = require("express");
 
 const port = process.env.PORT || 5000;
 // middle ware
 app.use(cors());
-// app.use(express.json())
+app.use(express.json());
 
 // Mongodb connect
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zighg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 async function run() {
   try {
     await client.connect();
-    console.log("Database connect succesfully")
-  } finally {
+    const database = client.db("doctors_portal");
+    const appointmentCollection = database.collection("appointments");
+
+    // post method
+    app.post("/appointments", async (req, res) => {
+      const appointment = req.body;
+      const result = await appointmentCollection.insertOne(appointment);
+      console.log(result);
+      res.json(result)
+    });
   
+  } finally {
     // await client.close();
   }
 }
 run().catch(console.dir);
 
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(` server is listening at http://localhost:${port}`)
-})
+  console.log(` server is listening at http://localhost:${port}`);
+});
